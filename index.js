26f87847912718033b357d60f110a2902e30d567
Ramiro <rhuayller@agetic.gob.bo>
2017-09-28 19:42:04 -0400

    avanzando

--- index.js
+++ index.js
@@ -2,36 +2,42 @@
   var nits = require('./data/nits2');
   var models = require('./models');
   var util = require('./util/util');
+  var timeout = 1000;
 
   // var nit = '295671015';
-models.empresa.findAll()
-.then(function (params) {
-  console.log("params", params)
-})
-.catch(function (params) {
-  console.log("errr", params)
-})
-
-
-
-util.iterarArray(nits, function (nit, callbackContinuar, callbackError) {
-
-
-  servicios.obtenerMatriculas(nit)
-    .then(function (resp) {
-      console.log(resp);
-      callbackContinuar()
+  models.empresa.findAll({
+      order: [
+        ['id_empresa', 'DESC']
+      ],
+      limit: 1,
+      raw: true
     })
-    .catch(function (respErr) {
-      console.log(respErr);
-      callbackContinuar()
+    .then(function (params) {
+      if (params.length > 0) {
+        console.log('params.nit', params[0].nit);
+      } else {
+        console.log("no hay nada. primera vez")
+        util.iterarArray(nits, function (nit, callbackContinuar, callbackError) {
+            setTimeout(function () {
+              servicios.obtenerMatriculas(nit)
+                .then(function (resp) {
+                  console.log(resp);
+                  callbackContinuar();
+                })
+                .catch(function (respErr) {
+                  console.log(respErr);
+                  callbackContinuar();
+                });
+            }, timeout);
+          })
+          .then(function (res) {
+            console.log('TERMINADO');
+          })
+          .catch(function (resErr) {
+            console.log('err ', resErr);
+          });
+      }
     })
-})
-.then(function (res) {
-  console.log("TERMINADO")
-})
-.catch(function (resErr) {
-  console.log("err ", resErr)
-})
-
-
+    .catch(function (params) {
+      console.log('errr', params);
+    });
