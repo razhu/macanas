62e1117195de460fb2ed22e3ff0c8c21ff2afd7a
Ramiro <rhuayller@agetic.gob.bo>
2017-09-28 21:52:27 -0400

    ok

--- index.js
+++ index.js
@@ -14,23 +14,20 @@
     })
     .then(function (params) {
       if (params.length > 0) {
-        console.log('params.nit', params[0].nit);
+        console.log('Problemas. Se reiniciara despues del NIT:', params[0].nit);
         var nitsFinal = nits.slice(nits.indexOf(params[0].nit), nits.length);
         // aqu copiar lo que eta por else. reemplazando nitsFinal
-      } else {
-        console.log('no hay nada. primera vez');
-        util.iterarArray(nits, function (nit, callbackContinuar, callbackError) {
+        util.iterarArray(nits, function (nitsFinal, callbackContinuar, callbackError) {
             setTimeout(function () {
               servicios.obtenerMatriculas(nit)
                 .then(function (resp) {
-                  console.log(resp)
                   if (resp.matriculas.length > 0) { //tiene matriculas
                     // inicio iterar por matriculas
                     resp.matriculas.forEach(function (objMatricula) {
                       setTimeout(function () {
+                        console.log("NIT / Matricula : ", nit + " " + objMatricula.matricula)
                         servicios.obtenerInformacionEmpresa(objMatricula.matricula)
                           .then(function (respM) {
-                            console.log('matricujla ', respM);
                             var estado = '';
                             typeof respM === 'object' ? estado = 'ACTIVO' : estado = 'INACTIVO';
                             models.empresa.create({
@@ -48,6 +45,61 @@
                     });
                     // fin iterar por matriculas
                   } else { //no tiene matricualas
+                    console.log("NIT :", nit)
+                    models.empresa.create({
+                      nit,
+                      estado: 'SIN_MATRICULA'
+                    });
+                  }
+                  callbackContinuar();
+                })
+                .catch(function (respErr) {
+                  console.log(respErr);
+                  callbackContinuar();
+                });
+            }, timeout2);
+          })
+          .then(function (res) {
+            console.log('TERMINADO');
+          })
+          .catch(function (resErr) {
+            console.log('err ', resErr);
+          });
+
+
+
+        
+      } else {
+        console.log('no hay nada. primera vez');
+        util.iterarArray(nits, function (nit, callbackContinuar, callbackError) {
+            setTimeout(function () {
+              servicios.obtenerMatriculas(nit)
+                .then(function (resp) {
+                  if (resp.matriculas.length > 0) { //tiene matriculas
+                    // inicio iterar por matriculas
+                    resp.matriculas.forEach(function (objMatricula) {
+                      setTimeout(function () {
+                        console.log("NIT / Matricula : ", nit + " " + objMatricula.matricula)
+                        servicios.obtenerInformacionEmpresa(objMatricula.matricula)
+                          .then(function (respM) {
+                            var estado = '';
+                            typeof respM === 'object' ? estado = 'ACTIVO' : estado = 'INACTIVO';
+                            models.empresa.create({
+                              nit,
+                              matricula_comercio: objMatricula.matricula,
+                              resultado: respM,
+                              estado
+                            });
+                            callbackContinuar();
+                          })
+                          .catch(function (respErr) {
+                            callbackContinuar();
+                          });
+                      }, timeout);
+                    });
+                    // fin iterar por matriculas
+                  } else { //no tiene matricualas
+                    console.log("NIT :", nit)
                     models.empresa.create({
                       nit,
                       estado: 'SIN_MATRICULA'
