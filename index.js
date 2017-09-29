4a8ed930f4f1c14c85ade283900afa991729f5c9
Ramiro <rhuayller@agetic.gob.bo>
2017-09-28 20:52:43 -0400

    ok

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
   var nits = require('./data/nits2');
   var models = require('./models');
   var util = require('./util/util');
-  var timeout = 1000;
+  var timeout = 2000;
+  var timeout2 = 1000;
 
   models.empresa.findAll({
       order: [
@@ -25,31 +26,26 @@
                   console.log(resp)
                   if (resp.matriculas.length > 0) { //tiene matriculas
                     // inicio iterar por matriculas
-                    util.iterarArray(resp.matriculas, function (objMatricula, callbackContinuar, callbackError) {
-                        setTimeout(function () {
-                          servicios.obtenerInformacionEmpresa(objMatricula.matricula)
-                            .then(function (respM) {
-                              console.log('matricujla ', respM);
-                              var estado = '';
-                              typeof respM === 'object' ? estado = 'ACTIVO' : estado = 'INACTIVO';
-                              models.empresa.create({
-                                nit,
-                                matricula_comercio: objMatricula.matricula,
-                                resultado: respM,
-                                estado
-                              });
-                              callbackContinuar();
-                            })
-                            .catch(function (respErr) {
-                              callbackContinuar();
+                    resp.matriculas.forEach(function (objMatricula) {
+                      setTimeout(function () {
+                        servicios.obtenerInformacionEmpresa(objMatricula.matricula)
+                          .then(function (respM) {
+                            console.log('matricujla ', respM);
+                            var estado = '';
+                            typeof respM === 'object' ? estado = 'ACTIVO' : estado = 'INACTIVO';
+                            models.empresa.create({
+                              nit,
+                              matricula_comercio: objMatricula.matricula,
+                              resultado: respM,
+                              estado
                             });
-                        }, timeout);
-                      })
-                      .then(function (res) {
-                      })
-                      .catch(function (resErr) {
-                        console.log('err ', resErr);
-                      });
+                            callbackContinuar();
+                          })
+                          .catch(function (respErr) {
+                            callbackContinuar();
+                          });
+                      }, timeout);
+                    });
                     // fin iterar por matriculas
                   } else { //no tiene matricualas
                     models.empresa.create({
@@ -57,14 +53,13 @@
                       estado: 'SIN_MATRICULA'
                     });
                   }
-
                   callbackContinuar();
                 })
                 .catch(function (respErr) {
                   console.log(respErr);
                   callbackContinuar();
                 });
-            }, timeout);
+            }, timeout2);
           })
           .then(function (res) {
             console.log('TERMINADO');
