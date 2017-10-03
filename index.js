b23bb87b98c4f286696547cf02ba53472871dcd3
Ramiro <rhuayller@agetic.gob.bo>
2017-10-03 16:12:15 -0400

    ok

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
   let servicios = require('./interoperabilidad/servicios_web');
   let nits = require('./data/nits2');
   let models = require('./models');
-  let util = require('./util/util');
-  let timeout = 15000;
-  let timeout2 = 5000;
+  let timeout = 60000;
 
   models.empresa.findAll({
       order: [
@@ -17,44 +15,44 @@
         // var nitsFinal = nits.slice(nits.indexOf(params[0].nit), nits.length);
         // // aqu copiar lo que eta por else. reemplazando nitsFinal
       } else {
-        async function nitf(elNit) {//async to obtain matriculas for a nit
+        async function nitf(elNit) { //async to obtain matriculas for a nit
           return await servicios.obtenerMatriculas(elNit)
         }
-        async function matrf(laMatr) {//async to obtain info business for a matricula
+        async function matrf(laMatr) { //async to obtain info business for a matricula
           return await servicios.obtenerInformacionEmpresa(laMatr)
         }
-        nits.forEach(function (nit) {
-          setTimeout(function() {
-          nitf(nit)
-            .then(resNit => {
-              if (resNit.matriculas.length > 0) { //tiene matriculas
-                resNit.matriculas.forEach(function (matri) {
-                  matrf(matri.matricula)
-                    .then(resMatr => {
-                      models.empresa.create({
-                        nit,
-                        nit_json: resNit,
-                        matricula_comercio: matri.matricula,
-                        matricula_comercio_json: resMatr,
-                        estado: "ACTIVO"
+        nits.forEach(function (nit, i) {
+          setTimeout(function () {
+            nitf(nit)
+              .then(resNit => {
+                if (resNit.matriculas.length > 0) { //tiene matriculas
+                  resNit.matriculas.forEach(function (matri) {
+                    matrf(matri.matricula)
+                      .then(resMatr => {
+                        models.empresa.create({
+                          nit,
+                          nit_json: resNit,
+                          matricula_comercio: matri.matricula,
+                          matricula_comercio_json: resMatr,
+                          estado: "ACTIVO"
+                        })
                       })
-                    })
-                    .catch(resMatrErr => {
-                      console.log("resMatrErr ", resMatrErr);
-                    })
-                });
-              } else { //no tiene matriculas
-                models.empresa.create({
-                  nit,
-                  nit_json: resNit,
-                  estado: "SIN_MATRICULA"
-                })
-              }
-            })
-            .catch(err => {
-              console.log('err ', err);
-            })
-          }, timeout);
+                      .catch(resMatrErr => {
+                        console.log("resMatrErr ", resMatrErr);
+                      })
+                  });
+                } else { //no tiene matriculas
+                  models.empresa.create({
+                    nit,
+                    nit_json: resNit,
+                    estado: "SIN_MATRICULA"
+                  })
+                }
+              })
+              .catch(err => {
+                console.log('err ', err);
+              })
+          }, timeout * (i + 1));
         });
       }
     })
